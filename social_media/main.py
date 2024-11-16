2c675f0801a7b3f8b69c2496f2127e9257752374
NoelTW <as11111satw@gmail.com>
2024-11-16 18:38:14 +0800

    create logging for all endpoints

--- social_media/main.py
+++ social_media/main.py
@@ -1,15 +1,18 @@
+import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
 from social_media.database import database
+from social_media.logging_config import configure_logging
 from social_media.routers.post import router as post_router
 
-app = FastAPI()
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    configure_logging()
     await database.connect()
     yield
     await database.disconnect()
--- social_media/routers/post.py
+++ social_media/routers/post.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import APIRouter, HTTPException
 
 from social_media.database import comment_table, database, post_table
@@ -11,13 +13,13 @@ from social_media.models.post import (
 
 router = APIRouter()
 
-# in-memory table
-# post_table = {}
-# comment_table = {}
+logger = logging.getLogger(__name__)
 
 
 async def find_post(post_id: int):
+    logger.info(f"Finding post with id {post_id}")
     query = post_table.select().where(post_table.c.id == post_id)
+    logger.debug(query)
     return await database.fetch_one(query)
 
 
@@ -31,31 +33,40 @@ async def create_post(post: UserPostIn):
 
 @router.get("/post", response_model=list[UserPost])
 async def get_all_posts():
+    logger.info("Getting all posts")
     query = post_table.select()
+    logger.debug(query)
     return await database.fetch_all(query)
 
 
 @router.post("/comment", response_model=Comment, status_code=201)
 async def create_comment(comment: CommentIn):
+    logger.info("Creating comment")
     post = await find_post(post_id=comment.post_id)
     if not post:
+        logger.error(f"Post with id {comment.post_id} not found!")
         raise HTTPException(status_code=404, detail="Post not found")
     data = comment.model_dump()
     query = comment_table.insert().values(data)
+    logger.debug(query)
     last_record_id = await database.execute(query)
     return {**data, "id": last_record_id}
 
 
 @router.get("/post/{post_id}/comment", response_model=list[Comment])
 async def get_comments_on_post(post_id: int):
+    logger.info("Getting comments on post")
     query = comment_table.select().where(comment_table.c.post_id == post_id)
+    logger.debug(query)
     return await database.fetch_all(query)
 
 
 @router.get("/post/{post_id}", response_model=UserPostWithComments)
 async def get_post_with_comments(post_id: int):
+    logger.info("Getting post and its all comments")
     post = await find_post(post_id=post_id)
     if not post:
+        logger.error(f"Post with id {post_id} not found!")
         raise HTTPException(status_code=404, detail="Post not found")
     """
     class UserPostWithComments(BaseModel):
