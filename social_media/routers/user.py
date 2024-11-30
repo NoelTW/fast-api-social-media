ffc4c913de8c1ee34459cb62f367429163566569
NoelTW <as11111satw@gmail.com>
2024-11-30 21:48:11 +0800

    add endpoint for user comfirmation

--- social_media/routers/user.py
+++ social_media/routers/user.py
@@ -1,23 +1,24 @@
 import logging
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Request, status
 
 from social_media.database import database, user_table
 from social_media.models.user import UserIn
 from social_media.security import (
-    get_user,
-    hash_password,
     authenticate_user,
     create_access_token,
+    create_comfirmation_token,
+    get_subject_for_token_type,
+    get_user,
+    hash_password,
 )
-
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
 @router.post("/register", status_code=201)
-async def register(user: UserIn):
+async def register(user: UserIn, request: Request):
     if await get_user(user.email):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered"
@@ -26,7 +27,12 @@ async def register(user: UserIn):
     query = user_table.insert().values(email=user.email, password=hashed_password)
     logger.debug(query)
     await database.execute(query)
-    return {"detail": "User created successfully"}
+    return {
+        "detail": "User created. Please comfirm your email!",
+        "comfirmation": request.url_for(
+            "comfirm_email", token=create_comfirmation_token(user.email)
+        ),
+    }
 
 
 @router.post("/token")
@@ -34,3 +40,14 @@ async def login(user: UserIn):
     user = await authenticate_user(user.email, user.password)
     access_token = create_access_token(user.email)
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/comfirm/{token}")
+async def comfirm_email(token: str):
+    email = get_subject_for_token_type(token, "comfirmation")
+    query = (
+        user_table.update().where(user_table.c.email == email).values(comfirmed=True)
+    )
+    logger.debug(query)
+    await database.execute(query)
+    return {"detail": "Email confirmed successfully"}
--- social_media/tests/routers/test_user.py
+++ social_media/tests/routers/test_user.py
@@ -1,3 +1,4 @@
+from fastapi import Request, HTTPException
 import pytest
 from httpx import AsyncClient
 
@@ -15,7 +16,7 @@ async def test_register_user(async_client: AsyncClient):
     password = "1234"
     response = await register_user(async_client, email, password)
     assert response.status_code == 201
-    assert "User created successfully" in response.json()["detail"]
+    assert "User created. Please comfirm your email!" in response.json()["detail"]
 
 
 @pytest.mark.anyio
@@ -48,3 +49,31 @@ async def test_login_user(async_client: AsyncClient, registered_user: dict):
     )
     assert response.status_code == 200
     assert "access_token" in response.json()
+
+
+@pytest.mark.anyio
+async def test_comfirm_user(async_client: AsyncClient, mocker):
+    spy = mocker.spy(Request, "url_for")
+    await register_user(async_client, email="test@example.com", password="1234")
+    comfirmation_url = str(spy.spy_return)
+    response = await async_client.get(comfirmation_url)
+
+    assert response.status_code == 200
+    assert {"detail": "Email confirmed successfully"}.items() <= response.json().items()
+
+
+@pytest.mark.anyio
+async def test_comfirm_user_invalid_token(async_client: AsyncClient):
+    response = await async_client.get("users/comfirm/invalid_token")
+    assert response.status_code == 401
+
+
+@pytest.mark.anyio
+async def test_comfirm_user_expired(async_client: AsyncClient, mocker):
+    mocker.patch("social_media.security.comfirm_token_expire_minutes", return_value=-1)
+    spy = mocker.spy(Request, "url_for")
+    await register_user(async_client, email="test@example.com", password="1234")
+    comfirmation_url = str(spy.spy_return)
+    response = await async_client.get(comfirmation_url)
+    assert response.status_code == 401
+    assert response.json()["detail"] == "Token has expired"
